39c6c94e6f017c491cc6d6ba1061806aadc25262
diegofarias06 <diego.farias.farias@gmail.com>
2021-03-21 16:10:03 -0300

    adição de utils.py com funções gerais

---
+++ utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+def rbf(x, c, l):
+    return np.exp(-((x-c)**2)/(2*l))
+
+
+def RMSE(y, y_hat):
+    N = len(y)
+    return np.sqrt(np.sum((y - y_hat)**2)/N)
+
+
+def phi(x, mean_sample, var_sample):
+    phi_x = np.array([1] + [rbf(x[i], mean_sample[i], var_sample[i]) for i in range(len(x))])
+    return phi_x
